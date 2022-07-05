5e124adf03437b4bebf88b4253d0fe06b9a8dad2
panghwi <kanean99@naver.com>
2022-07-05 11:41:24 +0900

    create errorhandler

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ const app = express();
 app.use('/tweet', tweetRouter);
 app.use('/auth', authRouter);
 
+app.use((req, res, next) => {
+  return res.status(404).send('Not Found');
+});
+app.use((err, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
+
 app.listen(8080);

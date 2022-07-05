f33d89567eb7926eb164764c2787cd981ca9aac1
panghwi <kanean99@naver.com>
2022-07-05 11:36:33 +0900

    create tweetData

---
+++ data/tweet.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    id: 1,
+    text: 'hello',
+    createAt: Date,
+    name: 'panghwi',
+    username: 'an',
+    //url: string(optional),
+  },
+  {
+    id: 2,
+    text: 'hi',
+    createAt: Date,
+    name: 'kwanghwi',
+    username: 'Ahn',
+    //url: string(optional),
+  },
+];

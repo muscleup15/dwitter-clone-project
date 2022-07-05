deebb844777a9d58dc2cd8a67bb26105ea0f3add
panghwi <kanean99@naver.com>
2022-07-05 11:32:52 +0900

    router method 설정

--- router/tweet.js
+++ router/tweet.js
@@ -2,7 +2,26 @@ import express from 'express';
 const route = express.Router();
 
 route.get('/', (req, res, err) => {
+  //getalltweets
   res.sendStatus(200);
 });
 
+route.get('/:id', (req, res, err) => {
+  //gettweetsbyid
+  res.sendStatus(200);
+});
+
+route.get('/:username', (req, res, err) => {
+  //gettseetsbyusername
+  res.sendStatus(200);
+});
+
+route.post('/:id', (req, res, err) => {
+  //createtweets
+  res.sendStatus(200);
+});
+
+route.put('/:id', (req, res, next) => {
+  res.sendStatus(200);
+});
 export default route;

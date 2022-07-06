72d0675fdc827b52801645729bc725295a1efd4e
panghwi <kanean99@naver.com>
2022-07-06 12:33:39 +0900

    auth_panghwi_ver

---
+++ data/auth.js
@@ -0,0 +1,16 @@
+export let users = [
+  {
+    username: 'kwanghwi',
+    password: '12345',
+    name: 'kwanghwi',
+    email: 'ellie@gmail.com',
+    url: '',
+  },
+  {
+    username: 'panghwi',
+    password: '12345',
+    name: 'panghwi',
+    email: 'ellie@gmail.com',
+    url: '',
+  },
+];
--- router/auth.js
+++ router/auth.js
@@ -1,8 +1,41 @@
 import express from 'express';
+import { users } from '../data/auth.js';
+import 'express-async-errors';
+import jwt from 'jsonwebtoken';
 const route = express.Router();
+route.use(express.json());
 
-route.get('/', (req, res, err) => {
-  res.sendStatus(200);
+const secretkey = '&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRg';
+const expireDate = '2d';
+
+route.post('/signin', (req, res, err) => {
+  const { username, password } = req.body;
+  const isUserName = users.find((user) => user.username == username);
+  const isPw = users.find((user) => user.password == password);
+  if (isUserName == false) {
+    res.status(404).json({ message: `Invalid id or password` });
+  }
+  if (isPw == false) {
+    res.status(404).json({ message: `Invalid id or password` });
+  }
+  const token = jwt.sign(req.body, secretkey, { expiresIn: expireDate });
+  res.status(200).json(token);
 });
 
+route.post('/signup', (req, res, err) => {
+  const { username, password, name, email, url } = req.body;
+  const user = {
+    id: Date.now().toString(),
+    username,
+    password,
+    name,
+    email,
+    url,
+  };
+  users.unshift(user);
+  res.status(201).json(users);
+});
+route.get('/me', (req, res, err) => {
+  res.sendStatus(200);
+});
 export default route;
